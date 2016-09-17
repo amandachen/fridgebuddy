a5ff0158121f9deec51c13abe7dd9e737a47f49e
Michael Velez <MichaelVelez@dhcp-ccc-4203.eduroam.cornell.edu>
2016-09-17 14:33:24 -0400

    Lot's of changes mostly home page stuff

---
+++ www/js/addItem.js
@@ -0,0 +1,10 @@
+function addItem() {
+	prompt({
+		title: "Item Entry",
+   		template: 'Enter Food Item',
+    	inputType: 'Food Item',
+    	inputPlaceholder: 'Eg. Eggs, Milk, etc.'
+    }).then(function(res) {
+    	console.log('Your password is', res);
+    });
+}
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,82 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, $ionicPopup) {
+
+   $scope.showPrompt = function() {
+
+      var outputFood
+      var outputDate
+
+      var promptPopup = $ionicPopup.prompt({
+         title: 'Date',
+         template: 'Enter Date',
+         inputType: 'text',
+         inputPlaceholder: 'Eg. 9/17/16'
+      });
+
+      var promptPopup2 = $ionicPopup.prompt({
+         title: 'Food Entry',
+         template: 'Enter Food Item',
+         inputType: 'text',
+         inputPlaceholder: 'Eg. Eggs, Milk, etc.'
+      });
+
+      promptPopup.then(function(res) {
+         outputDate = console.log(res);
+         var lst = document.getElementById("ion-list");
+         var item = tab-dash.createElement("button");
+         item.appendChild(tab-dash.createTextNode(outputDate));
+         lst.appendChild(item);
+      });
+    
+     promptPopup2.then(function(res) {
+         outputFood = console.log(res);
+         var lst = document.getElementById("ion-list");
+         var item = document.createElement("button");
+         item.appendChild(document.createTextNode(outputFood));
+         lst.appendChild(item);
+      });
+   };
+
+})
+
+/*.controller('DashCtrl', function($scope, $ionicPopup) {
+
+   // When button is clicked, the popup will be shown...
+   $scope.showPopup = function() {
+      $scope.data = {}
+    
+      // Custom popup
+      var myPopup = $ionicPopup.show({
+         template: '<input type = "text" ng-model = "data.model">',
+         title: 'Food Entry',
+         subTitle: 'Enter Food Item',
+         scope: $scope,
+      
+         buttons: [
+            { text: 'Cancel' }, {
+               text: '<b>Save</b>',
+               type: 'button-positive',
+                  onTap: function(e) {
+            
+                     if (!$scope.data.model) {
+                        //don't allow the user to close unless he enters model...
+                           e.preventDefault();
+                     } else {
+                        return $scope.data.model;
+                     }
+                  }
+            }
+         ]
+      });
+
+      myPopup.then(function(res) {
+         var output = console.log(res);
+
+      });    
+   };
+
+})*/
 
 .controller('RecipesCtrl', function($scope) {})
 
