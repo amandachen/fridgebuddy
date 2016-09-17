7cf16741420d43b028435e407c457f4f3edd9cba
swaggie <amandac188@gmail.com>
2016-09-17 07:46:26 -0400

    made a change

--- www/js/app.js
+++ www/js/app.js
@@ -5,92 +5,47 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            cordova.plugins.Keyboard.disableScroll(true);
+app.service('Todo', function () {
 
-        }
-        if (window.StatusBar) {
-            // org.apache.cordova.statusbar required
-            StatusBar.styleDefault();
-        }
-    });
-})
+    var todos = [
+        {
+            'title': 'Test1',
+            'done': 'false'
+            },
+        {
+            'title': 'Test2',
+            'done': 'false'
+            },
+        {
+            'title': 'Test3',
+            'done': 'true'
+            },
+        {
+            'title': 'Test4',
+            'done': 'true'
+            }];
 
-.config(function ($stateProvider, $urlRouterProvider) {
+    this.list = function () {
+        return todos;
+    }
 
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
-    // Set up the various states which the app can be in.
-    // Each state's controller can be found in controllers.js
-    $stateProvider
-
-    // setup an abstract state for the tabs directive
-        .state('tab', {
-        url: '/tab',
-        abstract: true,
-        templateUrl: 'templates/tabs.html'
-    })
-
-    // Each tab has its own nav history stack:
-
-    .state('tab.dash', {
-        url: '/dash',
-        views: {
-            'tab-dash': {
-                templateUrl: 'templates/tab-dash.html',
-                controller: 'DashCtrl'
-            }
-        }
-    })
-
-    .state('tab.chats', {
-            url: '/chats',
-            views: {
-                'tab-chats': {
-                    templateUrl: 'templates/tab-chats.html',
-                    controller: 'ChatsCtrl'
-                }
-            }
-        })
-        .state('tab.chat-detail', {
-            url: '/chats/:chatId',
-            views: {
-                'tab-chats': {
-                    templateUrl: 'templates/chat-detail.html',
-                    controller: 'ChatDetailCtrl'
-                }
-            }
-        })
-
-    .state('tab.recipes', {
-        url: '/recipes',
-        views: {
-            'tab-recipes': {
-                templateUrl: 'templates/tab-recipes.html',
-                controller: 'RecipesCtrl'
-            }
-        }
-    })
-
-
-    .state('tab.account', {
-        url: '/account',
-        views: {
-            'tab-account': {
-                templateUrl: 'templates/tab-account.html',
-                controller: 'AccountCtrl'
-            }
-        }
-    });
-
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/tab/dash');
+    this.add = function (todo) {
+        todos.splice(0, 0, todo);
+    }
 
 });
+
+app.controller('todoController', ['$scope', 'Todo',
+function ($scope, Todo) {
+
+        $scope.todos = Todo.list();
+        $scope.addTodo = function (todo) {
+            todo.done = false;
+            Todo.add(angular.copy(todo));
+            todo.title = '';
+
+        }
+
+}
+]);
