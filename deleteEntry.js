8806ff38bbba67243d5384c3c98e47bda8f6a262
swathi917 <swathichakrapani97@gmail.com>
2016-09-17 21:23:51 -0400

    DELETE ENTRY FINALLY WORKS!

--- deleteEntry.js
+++ deleteEntry.js
@@ -8,16 +8,16 @@ function deleteEntry(){
 var r = confirm("Do you want to delete this entry?");
 if (r == true) {
     var gparent = document.getElementById("ion-content");
-    var parent = document.getElementById("FoodButton");
+    var parent = document.getElementById("FoodButtons");
 	var child = document.getElementById("eggos");
 	var list = document.getElementById("myList");
 	console.log(gparent);
 
 	console.log(parent);
 	console.log(child);
-	var cNode = document.createTextNode(child);
+	//var cNode = document.createElement(child);
 	var pNode = document.createTextNode(parent);
-	pNode.removeChild(cNode);
+	parent.removeChild(child);
 	
 }
 else {}
