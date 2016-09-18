099e587006cbc4bec9221b16b7493dcf83b51144
Michael Velez <MichaelVelez@dhcp-ccc-4203.eduroam.cornell.edu>
2016-09-17 20:33:10 -0400

    I need to finish deleting stuff

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,6 +6,7 @@ angular.module('starter.controllers', [])
 
       var outputFood
       var outputDate
+      var foodItem
 
       var promptPopup = $ionicPopup.prompt({
          title: 'Date',
@@ -22,61 +23,43 @@ angular.module('starter.controllers', [])
       });
 
       promptPopup.then(function(res) {
-         outputDate = console.log(res);
-         var lst = document.getElementById("ion-list");
-         var item = tab-dash.createElement("button");
-         item.appendChild(tab-dash.createTextNode(outputDate));
-         lst.appendChild(item);
+         outputDate = res;
+         var lst = document.getElementById("food-button");
+         var item = document.createElement("item-note");
+         outputDate = item.appendChild(document.createTextNode(outputDate));
+         foodItem.appendChild(outputDate);
       });
     
      promptPopup2.then(function(res) {
-         outputFood = console.log(res);
-         var lst = document.getElementById("ion-list");
+         outputFood = res;
+         var lst = document.getElementById("food-button");
          var item = document.createElement("button");
          item.appendChild(document.createTextNode(outputFood));
+         foodItem = item;
          lst.appendChild(item);
       });
    };
 
-})
-
-/*.controller('DashCtrl', function($scope, $ionicPopup) {
-
-   // When button is clicked, the popup will be shown...
-   $scope.showPopup = function() {
-      $scope.data = {}
-    
-      // Custom popup
-      var myPopup = $ionicPopup.show({
-         template: '<input type = "text" ng-model = "data.model">',
-         title: 'Food Entry',
-         subTitle: 'Enter Food Item',
-         scope: $scope,
-      
-         buttons: [
-            { text: 'Cancel' }, {
-               text: '<b>Save</b>',
-               type: 'button-positive',
-                  onTap: function(e) {
-            
-                     if (!$scope.data.model) {
-                        //don't allow the user to close unless he enters model...
-                           e.preventDefault();
-                     } else {
-                        return $scope.data.model;
-                     }
-                  }
-            }
-         ]
+   $scope.showConfirm = function() {
+      var id 
+      function reply_click(clicked_id) {
+        id = clicked_id;
+      }
+      var confirmPopup = $ionicPopup.confirm({
+         title: console.log(id),
+         template: 'Do you want to delete this item?'
       });
 
-      myPopup.then(function(res) {
-         var output = console.log(res);
-
-      });    
+      confirmPopup.then(function(res) {
+         if(res) {
+            console.log('Sure!');
+         } else {
+            console.log('Not sure!');
+         }
+      });
+    
    };
-
-})*/
+})
 
 .controller('RecipesCtrl', function($scope) {})
 
@@ -99,8 +82,27 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
+.controller('AccountCtrl', function($scope, $ionicPopup) {
   $scope.settings = {
     enableFriends: true
   };
+
+  $scope.showConfirm = function() {
+     
+      var confirmPopup = $ionicPopup.confirm({
+         title: 'Reset Fridge',
+         template: 'Are you sure you want to do this?'
+      });
+
+      confirmPopup.then(function(res) {
+         if(res) {
+            console.log('Sure!');
+            var lst = document.getElementById("food-button");
+            lst.length = 0;
+         } else {
+            console.log('Not sure!');
+         }
+      });
+    
+   };
 });
